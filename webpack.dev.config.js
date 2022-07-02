07c64d06ac984d69d9545f88cdb8940f39624bf0
jrichardsz <jrichardsz.java@gmail.com>
2022-07-01 20:43:22 -0500

    link start

---
+++ webpack.dev.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: ["./src/index.js"],
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public",
+    publicPath: "/"
+  },
+  module: {
+    rules: [{
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    // CleanWebpackPlugin will do some clean up/remove folder before build
+    // In this case, this plugin will remove 'dist' and 'build' folder before re-build again
+    new CleanWebpackPlugin(),
+    // The plugin will generate an HTML5 file for you that includes all your webpack bundles in the body using script tags
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+  ],
+  resolve: {
+    fallback: {
+      "fs": false
+    },
+    alias: {
+      ejs_components: __dirname + '/node_modules/ejs'
+    }
+  }
+};
